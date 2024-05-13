6fd2c4ab50f9236b32782822e925a8e76fb8918a
Angelique <angelique.copere@gmail.com>
2024-05-13 17:04:25 +0200

    update dependency babel & home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-
 function App() {
   return (
-    <div>
-      <h1 className="text-4xl text-yellow font-dancing-script">Mon Food-truck</h1>
-    </div>
+    <main>
+      <section className="flex flex-col items-center">
+        <h1 className="text-yellow font-dancing-script text-5xl pt-10 tracking-wide text-center">
+          Mon food truck
+        </h1>
+        <p className="text-yellow text-2xl font-dancing-script tracking-wide">
+          Découvrez notre concept !
+        </p>
+        <img className="size-8 mt-4 animate-pulse" src="./arrow-right-solid.svg" alt="arrow" />
+      </section>
+    </main>
   );
 }
 
