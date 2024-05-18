a946a43f75e44e106efb36dc962e1994883cb035
Angelique <angelique.copere@gmail.com>
2024-05-18 18:21:58 +0200

    delete scroll horizontal mobile

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -19,27 +19,29 @@ export default function CardProduct({ items }) {
   }
 
   return (
-    <div className='relative w-[330px] h-[360px] overflow-x-auto'>
+    <div className='relative w-[330px] h-[360px]'>
       <div className='hidden md:block'>
         <img className='absolute top-[50%] size-5 hover:scale-125' onClick={prevSlide} src="../arrow-left-solid.svg" alt="flèche gauche" />
       </div>
-        <div className='flex bg-gray rounded-lg shadow-around mx-[22px]'>
-          {
-            items.map((item, index) => (
-              <div key={index} className={`rounded-lg bg-gray-200 flex flex-col items-center p-4 font-roboto transition-opacity duration-500 ${index === currentIndex ? 'opacity-100' : 'opacity-0'} ${index === currentIndex ? 'block' : 'hidden'}`}>
-                <img className='w-52 h-36 rounded-lg' src={item.imgSrc} alt={item.name} />
-                <p className='font-bold tracking-wide text-lg mt-2'>{item.name}</p>
-                <p className='text-center mt-1'>{item.description}</p>
-                <span className='border-2 border-orange rounded-md bg-orange text-black font-semibold tracking-wide mt-2 py-1 px-2'>{item.price} € avec garnitures</span>
-                <div className='flex gap-2 mt-3'>
-                  {items.map((_, subIndex) => {
-                    return <i key={subIndex} onClick={() => handleBurger(subIndex)} className={`fa-solid fa-circle ${subIndex === currentIndex ? "circleOrange" : ""}`}></i>
-                  })}
-                </div>
+      <div className=' bg-gray rounded-lg shadow-around mx-[22px]'>
+
+        {
+          items.map((item, index) => (
+            <div key={index} className={`rounded-lg bg-gray-200 flex flex-col items-center p-4 font-roboto transition-opacity duration-500 ${index === currentIndex ? 'opacity-100' : 'opacity-0'} ${index === currentIndex ? 'block' : 'hidden'}`}>
+              <img className='w-52 h-36 rounded-lg' src={item.imgSrc} alt={item.name} />
+              <p className='font-bold tracking-wide text-lg mt-2'>{item.name}</p>
+              <p className='text-center mt-1'>{item.description}</p>
+              <span className='border-2 border-orange rounded-md bg-orange text-black font-semibold tracking-wide mt-2 py-1 px-2'>{item.price} € avec garnitures</span>
+              <div className='flex gap-2 mt-3'>
+                {items.map((_, subIndex) => {
+                  return <i key={subIndex} onClick={() => handleBurger(subIndex)} className={`fa-solid fa-circle ${subIndex === currentIndex ? "circleOrange" : ""}`}></i>
+                })}
               </div>
-            ))
-          }
-        </div>
+            </div>
+          ))
+        }
+
+      </div>
       <div className='hidden md:block'>
         <img className='absolute top-[50%] right-0 size-5 hover:scale-125' onClick={nextSlide} src="../arrow-right-solid.svg" alt="flèche droite" />
       </div>
