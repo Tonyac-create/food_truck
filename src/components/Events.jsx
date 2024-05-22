a9def483993f4c290cf189a5b4896ac96602afd3
Angelique <angelique.copere@gmail.com>
2024-05-22 14:44:42 +0200

    clean index.css & update responsive

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -4,11 +4,13 @@ export default function Events() {
     return (
         <section id='events' className='flex flex-col items-center m-6'>
             <h2 className='text-yellow font-dancing-script text-5xl tracking-wide mb-4'>Evénements passés</h2>
-            <img className='rounded-xl my-4' src="../pictures/food_truck.jpg" alt="Food truck" />
-            <ul className='text-gray text-justify'>
-                <li><span className='font-medium text-yellow'>Festival Musique</span> / 20 Février 2024</li>
-                <li><span className='font-medium text-yellow'>Anniversaire Milou</span> / 11 et 12 Mai 2024</li>
-            </ul>
+            <div className='lg:flex lg:flex-row lg:justify-center lg:items-center lg:gap-10'>
+                <img className='rounded-xl my-4' src="../pictures/food_truck.jpg" alt="Food truck" />
+                <ul className='text-gray text-justify'>
+                    <li><span className='font-medium text-yellow'>Festival Musique</span> / 20 Février 2024</li>
+                    <li><span className='font-medium text-yellow'>Anniversaire Milou</span> / 11 et 12 Mai 2024</li>
+                </ul>
+            </div>
         </section>
     )
 }
--- src/components/Informations.jsx
+++ src/components/Informations.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Informations() {
     return (
         <section id='informations' className='m-6 pt-24 md:flex md:flex-row'>
-            <div className='md:flex: md:flex-col md:p-4 md:w-1/2 lg:p-6 xl:p-8 2xl:p-12'>
+            <div className='md:flex md:flex-col md:justify-center md:p-4 md:w-1/2 lg:p-6 xl:p-8 2xl:p-12'>
                 <p className='text-gray mb-5 text-justify'>Venez découvrir une cuisine authentique préparée avec des ingrédients frais: steak charolais 150g,
                     grenouilles fraiches. Pas envie de cuisiner, le food truck est là pour vous régaler.</p>
                 <div className='text-gray'>
@@ -22,7 +22,7 @@ export default function Informations() {
             <div className='md:flex md:flex-col md:w-1/2 md:px-4 lg:p-6'>
                 <p className='text-gray text-center'>Commandez au <span className='text-yellow font-semibold tracking-wide'>01.02.03.04.05</span></p>
                 <p className='text-gray text-center mt-4'>Privatisez le food truck pour vos événements</p>
-                <img className='hidden md:block md:rounded-md md:mt-1' src="../pictures/cuisinier_N&B.jpg" alt="cuisinier" />
+                <img className='hidden md:block md:rounded-md md:mt-5' src="../pictures/cuisinier_N&B.jpg" alt="cuisinier" />
             </div>
         </section>
     )
